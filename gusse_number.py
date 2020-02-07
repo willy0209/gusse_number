74e675d8ee34c9f438dd0c5f7c441d75ae229b07
Willy Lee <mnak0209@gmail.com>
2020-02-07 21:09:28 +0800

    third_version

--- gusse_number.py
+++ gusse_number.py
@@ -1,10 +1,15 @@
 import random
-r = random.randint(1,100)
+start = input('請決定隨機數字的範圍開始值: ')
+end = input('請決定隨機數字的範圍結束值: ')
+start = int(start)
+end = int(end)
+
+r = random.randint(start, end)
 count = 0
 
 while True:
 	count = count + 1
-	num = input('請猜數字1~100: ')	
+	num = input('請猜數字: ')	
 	num = int(num)
 	if num == r :
 		print('恭喜你答對了')
